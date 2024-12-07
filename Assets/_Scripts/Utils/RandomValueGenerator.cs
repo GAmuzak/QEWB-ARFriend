b1105caa1df251c23ce3b03ddd16529f10dd4dde
GAmuzak <rishavbanerjee1710@gmail.com>
2024-12-07 07:52:19 -0800

    initial commit with model and basic utils setup

---
+++ Assets/_Scripts/Utils/RandomValueGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class RandomValueGenerator
+{
+    public static List<float> GenerateValues(int count, float mean, float min, float max, Random random)
+    {
+        if (count <= 0) throw new ArgumentException("Number of values must be greater than 0.");
+        if (min > mean || mean > max) throw new ArgumentException("The target average must be between the lower and upper bounds.");
+
+        List<float> numbers = new List<float>();
+
+        if (count % 2 != 0)
+        {
+            numbers.Add(mean);
+            count--;
+        }
+
+        for (int i = 0; i < count/2; i++)
+        {
+            float n = random.NextFloat(min, max);
+            numbers.Add(n);
+            float diff = n - mean;
+            numbers.Add(mean - diff);
+        }
+
+        numbers.Shuffle(random);
+        return numbers;
+    }
+
+}
